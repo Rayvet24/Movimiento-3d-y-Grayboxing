2addd506569acb469a12cdc0628f9b51b4dd8701
Rayvet24 <elias.ivan.rodriguez2004@gmail.com>
2026-09-13 21:48:58 -0300

    Scrip de doble salto y rotar camara con teclas flecha izq y der

--- personaje.gd
+++ personaje.gd
@@ -2,15 +2,26 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
+var Double_Jump: bool
 
 func _physics_process(delta: float) -> void:
+	#Grevedad
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+	#Resetear doble salto
+	if is_on_floor():
+		Double_Jump = true
+	
+	#Salto normal
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	#Doble salto
+	if Input.is_action_just_pressed("Jump") and not is_on_floor() and Double_Jump == true:
+		velocity.y = JUMP_VELOCITY
+		Double_Jump = false
+	
+	#Movimiento básico
 	var input_dir := Input.get_vector("Left", "Right", "Up", "Down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -19,5 +30,11 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
+	#Rotar
+	if Input.is_action_just_pressed("Rotate cam left"):
+		rotate(Vector3.UP, 1 * delta)
+	if Input.is_action_just_pressed("Rotate cam right"):
+		rotate(Vector3.UP, -1 * delta)
+	
 	move_and_slide()
